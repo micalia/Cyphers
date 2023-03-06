6a17f668afde2370e4759d238a9ff2e11d6701b4
micalia <micalia0612@gmail.com>
2023-03-06 10:40:41 +0900

    카야 블렌드스페이스 적용 완료

---
+++ Source/Cyphers/Private/PlayerAnim.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerAnim.h"
+#include "Cypher_Kaya.h"
+#include <GameFramework/CharacterMovementComponent.h>
+
+void UPlayerAnim::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	APawn* pawn = TryGetPawnOwner();
+
+	me = Cast<ACypher_Kaya>(pawn);
+}
+
+void UPlayerAnim::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (me != nullptr)
+	{		
+		velocity = me->GetVelocity();
+		forward = me->GetActorForwardVector();
+		right = me->GetActorRightVector();
+
+		dirV = FVector::DotProduct(velocity, forward);
+		dirH = FVector::DotProduct(velocity, right);
+			
+		bAir = me->GetCharacterMovement()->IsFalling();
+	}
+}
---
+++ Source/Cyphers/Public/PlayerAnim.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnim.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class CYPHERS_API UPlayerAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY()
+	class ACypher_Kaya* me;
+
+	FVector velocity;
+	FVector forward;
+	FVector right;
+
+	// 앞, 뒤를 판별하는 변수
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		float dirV = 0;
+
+	// 오른쪽, 왼쪽을 판별하는 변수
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		float dirH = 0;
+
+	//공중에 있는지 여부
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		bool bAir = false;
+};
